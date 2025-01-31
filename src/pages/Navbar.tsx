ef0aadff5d5c9fb5aa3ed0faa9293082866dd2b1
Debasish Mahanta <73976256+debasish26@users.noreply.github.com>
2025-01-31 23:48:09 +0530

    Update Navbar.tsx

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -95,8 +95,8 @@ const Navbar = () => {
         <div className="md:hidden bg-gray-800 shadow-lg">
           <div className="px-2 pt-2 pb-3 space-y-1">
             <a href="/" className="text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
-            <a href="#" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Browse</a>
-            <a href="#" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">My List</a>
+            <a href="/genres" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Browse</a>
+{/*             <a href="#" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">My List</a> */}
             {isLoggedIn ? (
                 <>
                   <a href="/profile" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Profile</a>
